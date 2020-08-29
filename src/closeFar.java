5cc7f5782c69f4c25117675d7c0f6e8bb30c6877
Jan Gevaert <jan.gevaert@gmail.com>
2020-08-29 06:32:04 -0600

    Initial commit

---
+++ src/closeFar.java
@@ -0,0 +1,7 @@
+public class closeFar {
+  public boolean closeFar(int a, int b, int c) {
+    return ((Math.abs(a - b) <= 1 && Math.abs(a - c) >= 2) && Math.abs(b - c) >= 2 ||
+        (Math.abs(a - c) <= 1 && Math.abs(a - b) >= 2 && Math.abs(b - c) >= 2));
+  }
+
+}
